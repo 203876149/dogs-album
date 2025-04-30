dc1e43a68d5c201960cad24683c4829802654a9b
shlomo shwirts <negev505@gmail.com>
2025-04-30 23:38:35 +0300

    add request and search components with basic templates

---
+++ src/app/request/request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request',
+  imports: [],
+  templateUrl: './request.component.html',
+  styleUrl: './request.component.css',
+  standalone: true,
+})
+export class RequestComponent {
+
+}
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  imports: [],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+  standalone: true,
+})
+export class SearchComponent {
+
+}
