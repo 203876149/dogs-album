be45da563310271bee6c02411ef989267d157f57
shlomo shwirts <negev505@gmail.com>
2025-05-01 12:26:21 +0300

    feat: add loading indicator to search component and improve form handling

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,17 +1,18 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {SearchService} from './search.service';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
-import {map, Observable, of, switchMap} from 'rxjs';
+import {map, Observable, of, switchMap, tap} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-search',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatSelectModule, MatInputModule, AsyncPipe, MatCardModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatSelectModule, MatInputModule, AsyncPipe, MatCardModule, MatProgressBarModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
   standalone: true,
@@ -26,15 +27,18 @@ export class SearchComponent {
     breed: new FormControl('', [Validators.required]),
     count: new FormControl(0, [Validators.min(1), Validators.max(50), Validators.required, Validators.pattern(/^[0-9]+$/)]),
   });
+  loading = signal(false);
 
   images$: Observable<string[]> = this.form?.valueChanges.pipe(
+    tap(() => this.loading.set(true)),
     switchMap((value) =>
       (this.form.valid) ? this.searchService.getBreedImagesCollection(value.breed as string, value.count as number)
           .pipe(
             map((res) => res?.message || []),
           )
         : of([])
-      )
+      ),
+    tap(() => this.loading.set(false))
     )
 
 
