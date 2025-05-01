a1fda8667d42008c75df404a03c379fe4c2893f9
shlomo shwirts <negev505@gmail.com>
2025-05-01 12:15:32 +0300

    style: enhance search and tabs components with improved layout and styling

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -5,7 +5,7 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
-import {filter, map, Observable, switchMap} from 'rxjs';
+import {map, Observable, of, switchMap} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 
@@ -24,23 +24,23 @@ export class SearchComponent {
   allBreeds: Array<{ breed: string, subBreeds: string[] }> = this.activatedRoute.snapshot.data['allBreeds'];
   form = new FormGroup({
     breed: new FormControl('', [Validators.required]),
-    count: new FormControl(10, [Validators.min(1), Validators.max(50)]),
+    count: new FormControl(0, [Validators.min(1), Validators.max(50), Validators.required, Validators.pattern(/^[0-9]+$/)]),
   });
 
   images$: Observable<string[]> = this.form?.valueChanges.pipe(
-    filter((value) => this.form.valid && !!value?.breed),
-    switchMap((value) => this.searchService.getBreedImagesCollection(value.breed as string, value.count as number)
-      .pipe(
-        map(({message}) => message)
+    switchMap((value) =>
+      (this.form.valid) ? this.searchService.getBreedImagesCollection(value.breed as string, value.count as number)
+          .pipe(
+            map((res) => res?.message || []),
+          )
+        : of([])
       )
     )
-  )
+
 
   ngOnInit() {
-    console.log(this.allBreeds);
-    this.form?.valueChanges.subscribe((value) => {
-      console.log(value);
-    });
+    this.images$.subscribe(res => console.log('images:', res));
+
   }
 
 }
